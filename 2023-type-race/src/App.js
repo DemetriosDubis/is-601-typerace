49822895af25d805672ed86011a5afd08385beaa
Demetrios <ddubis2@my.stlcc.edu>
2023-07-05 13:22:04 -0500

    601 Project wk5

--- 2023-type-race/src/App.js
+++ 2023-type-race/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import React from 'react';
 
-function App() {
+
+const App = () => {
+  const buttonTextItems = ['bears, beets, battlestar galactica',
+"What's Forrest Gump's password? 1Forrest1",
+'Where do programmers like to hang out?  The Foo Bar'];
+const initialGameState = {
+  victory: false,
+  startTime: null,
+  endTime: null
+}
+      const [snippet, setSnippet] = useState('');
+      const [userText, setUserText] = useState('');
+      const [gameState, setGameState] = useState(initialGameState);
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+
+    if(event.target.value === snippet) {
+      setGameState({
+        gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  };
+  const chooseSnippet = index => {
+    setSnippet(buttonTextItems[index]);
+    setGameState({ gameState, startTime: new Date().getTime() });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ <h2>TypeRace</h2>
+ <hr />
+ <h3>Snippet</h3>
+ <div>{snippet}</div>
+ <h4>{gameState.victory ? `Done! Woot! Time: ${gameState.endTime}ms` : null}</h4>
+    <input value={userText} onChange={updateUserText} />
+    <hr />
+    {buttonTextItems.map((textItem, index) => <button onClick={() => chooseSnippet(index)}>{textItem}</button>)}
     </div>
   );
-}
+};
 
 export default App;
